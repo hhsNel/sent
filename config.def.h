4ec4cebebeaced6fb77acccd654c3ee1dd3426f2
hhsNel <hihijsonel@gmail.com>
2025-10-04 14:45:41 +0200

    patches

--- config.def.h
+++ config.def.h
@@ -13,6 +13,11 @@ static const char *colors[] = {
 	"#FFFFFF", /* background color */
 };
 
+static const char *inverted_colors[] = {
+	"#FFFFFF", /* foreground color */
+	"#000000", /* background color */
+};
+
 static const float linespacing = 1.4;
 
 /* how much screen estate is to be used at max for the content */
@@ -47,6 +52,7 @@ static Shortcut shortcuts[] = {
 	{ XK_n,           advance,        {.i = +1} },
 	{ XK_p,           advance,        {.i = -1} },
 	{ XK_r,           reload,         {0} },
+	{ XK_i,           togglescm,      {0} },
 };
 
 static Filter filters[] = {
